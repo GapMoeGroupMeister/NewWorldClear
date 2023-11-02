c80dee1349f9d44a1bfebad2202b2a828932c0d5
vcsHB <99899703+vcsHB@users.noreply.github.com>
2023-11-02 09:53:17 +0900

    Save

---
+++ Assets/02_Scripts/vcs/Inven/Slot.cs
@@ -0,0 +1,39 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+
+public class Slot : MonoBehaviour
+{
+    private Image ItemImage;
+    private Image GuageFill;
+    private TextMeshProUGUI ItemAmount;
+
+    private void Awake()
+    {
+        ItemImage = transform.Find("ItemImage").GetComponent<Image>();
+        GuageFill = transform.Find("ConditionGuage").transform.Find("GuageFill").GetComponent<Image>();
+        ItemAmount = transform.Find("AmountBG").transform.Find("AmountText").GetComponent<TextMeshProUGUI>();
+
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void SetSlot(ItemSlot slotInfo)
+    {
+        
+    }
+}
