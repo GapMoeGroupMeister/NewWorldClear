22442011d5374fe9d45c4ab5bb18c40cda794ed8
vcsHB <99899703+vcsHB@users.noreply.github.com>
2023-12-07 01:35:20 +0900

    feat. InGameDrop Item Test

---
+++ Assets/02_Scripts/vcs/Class/DropItem.cs
@@ -0,0 +1,19 @@
+﻿
+
+public class DropItem
+{
+    public string itemName;
+
+    public Item item;
+
+    public int amount;
+
+    /**
+     * <summary>
+     * 질량, 끌려오는 속도에 관여함
+     * </summary>
+     */
+    public float mass;
+
+
+}
