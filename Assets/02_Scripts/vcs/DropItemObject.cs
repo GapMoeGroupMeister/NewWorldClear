78db4e3249a954a8cecb9643647b6f6d809f4c40
vcsHB <99899703+vcsHB@users.noreply.github.com>
2023-12-10 02:58:38 +0900

    feat: DropItem

--- Assets/02_Scripts/vcs/DropItemObject.cs
+++ Assets/02_Scripts/vcs/DropItemObject.cs
@@ -3,41 +3,22 @@ using System.Collections;
 using Unity.VisualScripting;
 using UnityEngine;
 
-public class DropItemObject : MonoBehaviour
+public class DropItemObject : DropObject
 {
     [SerializeField] private DropItem _dropItem;
 
-    private bool PlayerTargeted = false;
-
-    [Range(0.5f, 5f)] [SerializeField] private float detectRange = 0.5f;
-
-    private Transform followTarget;
-    private Vector2 currentDirection;
-
-    public void TracePlayer()
+    private void Update()
     {
-        
-        
+        Update_Check();
     }
 
-
-    private void DetectPlayer()
-    {
-        // 플레이어 레이어 추가되면 설정
-        Collider2D[] hits =  Physics2D.OverlapCircleAll(transform.position, detectRange);
-
-        foreach (Collider2D collider in hits)
-        {
-            if (collider.CompareTag("Player"))
-            {
-                followTarget = collider.transform;
-            }
-        }
-    }
-
-    private IEnumerator FollowRoutine()
+    public override void Get()
     {
         
     }
+
+
+    
+    
     
 }
