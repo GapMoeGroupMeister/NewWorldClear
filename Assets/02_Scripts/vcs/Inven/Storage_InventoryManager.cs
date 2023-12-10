051a5146d2e83ae1d71c44a5305153535b54d380
vcsHB <99899703+vcsHB@users.noreply.github.com>
2023-12-10 14:15:51 +0900

    feat: InGame InventoryManager

--- Assets/02_Scripts/vcs/Inven/Storage_InventoryManager.cs
+++ Assets/02_Scripts/vcs/Inven/Storage_InventoryManager.cs
@@ -4,13 +4,9 @@ using System.Collections.Generic;
 using JetBrains.Annotations;
 using UnityEngine;
 
-public class Storage_InventoryManager : MonoBehaviour
+public class Storage_InventoryManager : InventoryManager
 {
-    [Tooltip("인벤슬롯 프리팹")]
-    [SerializeField] private GameObject slotPrefab;
-
-    [SerializeField] private Transform grid;
-
+    
     [SerializeField] private ItemSlot defaultItem;
     [SerializeField] private ItemSlot canSoup;
 
@@ -25,85 +21,40 @@ public class Storage_InventoryManager : MonoBehaviour
 
     void Start()
     {
+        SceneStart();
+    }
+
+
+
+    public override void SceneStart()
+    {
         ItemManager.Instance.LoadInventoryFile();
         Set_AllSlot();
     }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-
     
-    /**
-     * <summary>
-     * inventory 전체를 지웠다가 다시 만듬
-     * </summary>
-     */
-    private void Refresh()
-    {
-        Delete_Inven();
-        Set_AllSlot();
-        
-    }
-    private void Set_AllSlot()
-    {
-        List<ItemSlot> inventory = ItemManager.Instance.inventory;
-        if (inventory.Count <= 0)
-        {
-            return;
-
-        }
-        
-        foreach (ItemSlot _slot in inventory)
-        {
-            Item thisItem = _slot.item;
-
-            if (thisItem == null)
-            {
-                inventory.Remove(_slot);
-                return;
-            }
-            
-            Set_Slot(_slot);
-        }
-    }
-    private void Set_Slot(ItemSlot itemSlot)
-    {
-        GameObject slot = Instantiate(slotPrefab, grid);
-        slot.GetComponent<Slot>().SetSlot(itemSlot);
-
-    }
-
-    
-    
-
-
-    
-    private void Refresh_Setting()
-    {
-            
-    }
-    /**
-     * <summary>
-     * inventory 전체를 지움
-     * </summary>
-     */
-    private void Delete_Inven()
-    {
-        foreach (Transform child in grid)
-        {
-            Destroy(child.gameObject) ;
-        }
-    }
-    
-    public void SceneExit()
+    public override void SceneExit()
     {
         ItemManager.Instance.SaveInventoryFile();
         
     }
 
+    [ContextMenu("Debug_Refresh")]
+    private void DebugRefresh()
+    {
+        Refresh();
+    }
+
+    [ContextMenu("Debug_AddItem")]
+    private void DebugAddDefaultItem()
+    {
+        ItemManager.Instance.AddItem(defaultItem);
+    }
+    
+    [ContextMenu("Debug_AddCanSoupItem")]
+    private void DebugAddCanSoupItem()
+    {
+        ItemManager.Instance.AddItem(canSoup);
+    }
 
 
 
