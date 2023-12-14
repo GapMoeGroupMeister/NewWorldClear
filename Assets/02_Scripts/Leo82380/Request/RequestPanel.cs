8cde64e6b7342fca0a69a5cd79cfe585944a1b0c
LEE SANG GYU <tkdrb062@gmail.com>
2023-12-14 19:26:20 +0900

    feat : Item

--- Assets/02_Scripts/Leo82380/Request/RequestPanel.cs
+++ Assets/02_Scripts/Leo82380/Request/RequestPanel.cs
@@ -1,11 +1,9 @@
-using System;
 using System.Collections.Generic;
 using UnityEngine;
 using DG.Tweening;
 using TMPro;
 using UnityEngine.UI;
 using Random = UnityEngine.Random;
-using EasyJson;
 
 public class RequestPanel : MonoBehaviour
 {
@@ -30,11 +28,9 @@ public class RequestPanel : MonoBehaviour
     [SerializeField] private TMP_Text requestAmountText1;
     [SerializeField] private TMP_Text requestAmountText2;
     // 준 갯수 텍스트
-    [SerializeField] private TMP_Text giveAmountText1;
-    [SerializeField] private TMP_Text giveAmountText2;
+    [SerializeField] private TMP_Text[] giveAmountText;
     // 요청 아이템 이미지
-    [SerializeField] private Image requestImage1;
-    [SerializeField] private Image requestImage2;
+    [SerializeField] private Image[] requestImage;
     // 보유 갯수 부족 매터리얼
     [SerializeField] private Material lackMaterial;
 
@@ -67,28 +63,19 @@ public class RequestPanel : MonoBehaviour
 
     public void RequestSetup()
     {
-        
         day++;
         if(day > 3)
             day = 1;
         if (day % 3 == 0)
         {
             man.SetActive(true);
-            transform.position = new Vector3(0f, -10f, 0f);
-            transform.DOMoveY(-3.5f, duration).SetEase(ease);
+            RequestPositionSetUp();
             randomIndex = Random.Range(0, ingredients.Count);
             nowRequest = ingredients[randomIndex];
-            var request = nowRequest.requests;
-            ingredientText.text = nowRequest.request + "\n<size=40><color=#000000>목표: " + request[0].item.itemName + " " +
-                                  ingredients[randomIndex].requests[0].amount + "개, "  +
-                                  request[1].item.itemName + ingredients[randomIndex].requests[1].amount
-                                  + "개</color></size>";
-            saveInfo.nowRequestId = nowRequest.requestID;
-            DBManager.Save_userInfo(saveInfo);
-            requestAmountText1.text = "X" + request[0].amount;
-            requestAmountText2.text = "X" + request[1].amount;
+            RequestTextSetUp();
+            RequestSave();
             RequestTextSetup();
-            count = request[0].amount;
+            
             acceptButton.SetActive(true);
         }
         else
@@ -106,39 +93,70 @@ public class RequestPanel : MonoBehaviour
     public void RequestTextSetup()
     {
         RequestMaterialSetup(0);
-        giveAmountText1.text = giveAmount[0] + "/" + nowRequest.requests[0].amount;
         RequestMaterialSetup(1);
-        giveAmountText2.text = giveAmount[1] + "/" + nowRequest.requests[1].amount;
     }
-    
     private void RequestMaterialSetup(int index)
     {
         if (giveAmount[index] >= nowRequest.requests[index].amount)
         {
-            giveAmountText1.color = Color.green;
-            requestImage1.material = null;
+            giveAmountText[index].color = Color.green;
+            requestImage[index].material = null;
         }
         else
         {
-            giveAmountText1.color = Color.red;
-            requestImage1.material = lackMaterial;
+            giveAmountText[index].color = Color.red;
+            requestImage[index].material = lackMaterial;
         }
+        giveAmountText[index].text = giveAmount[index] + "/" + nowRequest.requests[index].amount;
+    }
+    private void RequestPositionSetUp()
+    {
+        transform.position = new Vector3(0f, -10f, 0f);
+        transform.DOMoveY(-3.5f, duration).SetEase(ease);
+    }
+    private void RequestTextSetUp()
+    {
+        var request = nowRequest.requests;
+        ingredientText.text = nowRequest.request + "\n<size=40><color=#000000>목표: " + request[0].item.itemName + " " +
+                              ingredients[randomIndex].requests[0].amount + "개, "  +
+                              request[1].item.itemName + ingredients[randomIndex].requests[1].amount
+                              + "개</color></size>";
+        requestAmountText1.text = "X" + request[0].amount;
+        requestAmountText2.text = "X" + request[1].amount;
+        count = request[0].amount;
+    }
+    private void RequestSave()
+    {
+        saveInfo.nowRequestId = nowRequest.requestID;
+        DBManager.Save_userInfo(saveInfo);
     }
 
     public void Request()
     {
         #nullable enable
-        ItemSlot? slot = ItemManager.Instance.FindItem(nowRequest.requests[0].item) 
-                         ?? ItemManager.Instance.FindItem(nowRequest.requests[0].item);
-        int? amount = slot.amount;
+        int? amount = FindItemSlot(nowRequest.requests[0].item).amount;
         requestPanel.transform.DOMoveY(0, duration).SetEase(ease);
-        print(slot.item);
+        print("a " +nowRequest.requests[0].item.itemSpriteName);
+        print("a " + nowRequest.requests[1].item.itemSpriteName);
+        IngredientImageSetup(0, 0);
+        IngredientImageSetup(1, 0);
+        IngredientImageSetup(2, 1);
+        IngredientImageSetup(3, 1);
+    }
+    
+    private void IngredientImageSetup(int imageIndex, int requestIndex)
+    {
+        ingredientImage[imageIndex].sprite = SpriteLoader.Instance.FindSprite(nowRequest.requests[requestIndex].item.itemSpriteName);
+        ingredientImage[imageIndex].SetNativeSize();
+        ingredientImage[imageIndex].transform.localScale *= 0.8f;
     }
 
     public void Pass()
     {
-        if (ItemManager.Instance.FindItem(nowRequest.requests[0].item) == null) return;
-        if (ItemManager.Instance.SubItem(nowRequest.requests[0].item, nowRequest.requests[0].amount))
+        if (FindItemSlot(nowRequest.requests[0].item) == null) return;
+        if (FindItemSlot(nowRequest.requests[0].item) == null) return;
+        if (ItemManager.Instance.SubItem(nowRequest.requests[0].item, nowRequest.requests[0].amount) &&
+            ItemManager.Instance.SubItem(nowRequest.requests[1].item, nowRequest.requests[1].amount))
         {
             requestPanel.transform.DOMoveY(-10f, duration).SetEase(ease);
             ingredientText.text = "감사합니다!\n<color=black><size=50>버튼을 눌러 돌아가기</size></color>";
@@ -146,9 +164,14 @@ public class RequestPanel : MonoBehaviour
         }
         else
         {
-            ingredientText.text = "재료가 부족합니다!\n<color=black><size=50>버튼을 눌러 돌아가기</size></color>";
-            acceptButton.SetActive(false);
+            print(nowRequest.requests[0].amount);
+            print(nowRequest.requests[1].amount);
         }
+    }
+    
+    private ItemSlot? FindItemSlot(Item item)
+    {
+        return ItemManager.Instance.FindItem(item);
     }
     
     public void RequestPanelClose()
