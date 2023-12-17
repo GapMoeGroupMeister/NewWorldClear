72dac3b1a9d5023dc5e35e79ddc4f9039b6bd1ba
vcsHB <99899703+vcsHB@users.noreply.github.com>
2023-12-17 15:54:31 +0900

    feat: UserrStatus, StartScene UI

---
+++ Assets/02_Scripts/vcs/StartScene/StartScene_UIManager.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class StartScene_UIManager : MonoBehaviour
+{
+    [SerializeField] private Image Gauge_Health;
+    [SerializeField] private Image Gauge_Hungery;
+    [SerializeField] private Image Gauge_Thirsty;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void RefreshStatusGauge()
+    {
+        
+    }
+}
