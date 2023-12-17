a49fd0e54927d2a9a64d05ec11a6a966561c944f
vcsHB <99899703+vcsHB@users.noreply.github.com>
2023-12-17 16:56:42 +0900

    feat. Connect Storage

--- Assets/02_Scripts/vcs/StartScene/StartScene_UIManager.cs
+++ Assets/02_Scripts/vcs/StartScene/StartScene_UIManager.cs
@@ -8,6 +8,7 @@ public class StartScene_UIManager : MonoBehaviour
     [SerializeField] private Image Gauge_Health;
     [SerializeField] private Image Gauge_Hungery;
     [SerializeField] private Image Gauge_Thirsty;
+
     
     // Start is called before the first frame update
     void Start()
