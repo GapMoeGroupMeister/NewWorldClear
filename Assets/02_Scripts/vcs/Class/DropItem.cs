16804746d5348228088e88782ebd453ae7b260af
vcsHB <99899703+vcsHB@users.noreply.github.com>
2023-12-20 20:10:53 +0900

    feat: ItemDrop
    base 병합 오류로 인해 날아간 부분 다시 넣음

---
+++ Assets/02_Scripts/vcs/Class/DropItem.cs
@@ -0,0 +1,28 @@
+﻿public class DropItem
+{
+    // 딱히 안쓸 예정
+    public string itemName;
+
+    /**
+     * <summary>
+     * 얻을 아이템
+     * </summary>
+     */
+    public Item item;
+
+    /**
+     * <summary>
+     * 얻을 아이템 개수
+     * </summary>
+     */
+    public int amount;
+
+    /**
+     * <summary>
+     * 질량, 끌려오는 속도에 관여함
+     * </summary>
+     */
+    public float mass;
+
+
+}
