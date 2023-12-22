452f178b4441cc8224452c330f710575bcfa3566
vcsHB <99899703+vcsHB@users.noreply.github.com>
2023-12-23 02:20:17 +0900

    feat: LootManager

---
+++ Assets/02_Scripts/vcs/InGameScene/LootManager.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LootManager : MonoSingleton<LootManager>
+{
+
+    [SerializeField] private GameObject DropItemPrefab;
+    [SerializeField] private GameObject DropExpPrefab;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    /**
+     * <param name="generatePos">
+     * 아이템 구슬을 생성할 위치
+     * </param>
+     * <param name="power">
+     * 생성위치에서 랜덤하게 퍼져나갈 세기
+     * </param>
+     * <summary>
+     * 경험치구슬을 생성하는 메서드
+     * </summary>
+     */
+    public void GenerateExp(Vector2 generatePos, float power)
+    {
+        
+    }
+}
