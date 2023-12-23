3b03bff4617a884f1ee8755b6a32551b67b13d99
vcsHB <99899703+vcsHB@users.noreply.github.com>
2023-12-23 18:45:41 +0900

    feat: Exp Prefab

--- Assets/02_Scripts/vcs/InGameScene/LootManager.cs
+++ Assets/02_Scripts/vcs/InGameScene/LootManager.cs
@@ -4,11 +4,15 @@ using UnityEngine;
 
 public class LootManager : MonoSingleton<LootManager>
 {
-
+    [Header("DropItemPrefab")]
     [SerializeField] private GameObject DropItemPrefab;
-    [SerializeField] private GameObject DropExpPrefab;
+    [Header("DropExpPrefabs")]
+    [SerializeField] private GameObject DropExp1Prefab;
+    [SerializeField] private GameObject DropExp5Prefab;
+    [SerializeField] private GameObject DropExp10Prefab;
+    [SerializeField] private GameObject DropExp50Prefab;
 
-
+    [Header("Debug")]
     [SerializeField] private Transform DebugPos;
 
     [SerializeField] private DropItem debugItem;
@@ -32,7 +36,37 @@ public class LootManager : MonoSingleton<LootManager>
      */
     public void GenerateExp(int expAmount, Vector2 generatePos, float power)
     {
+        int exp1 = expAmount;
+        int exp50 = NumberByUnit(ref exp1, 50);
+        int exp10 = NumberByUnit(ref exp1, 10);
+        int exp5 = NumberByUnit(ref exp1, 5);
         
+        //PoolManager.Instance.GetObject()
+        
+        DropItemObject dropItemObject = Instantiate(DropItemPrefab, generatePos, Quaternion.identity).GetComponent<DropItemObject>();
+        Vector2 randomPosition = Random.insideUnitCircle.normalized;
+        dropItemObject.AddForce(randomPosition, power);
+    }
+
+    /**
+     * <param name="num">
+     * 남은 값 (ref 참조형태)
+     * </param>
+     * <param name="unit">
+     * num값을 나누는 단위
+     * </param>
+     * <summary>
+     * 입력받은 num값을 단위로 나누어 반환하고 나머지를 저장하는 메서드
+     * </summary>
+     * <returns>
+     * 단위로 나눈 값을 반환
+     * </returns>
+     */
+    private int NumberByUnit(ref int num, int unit)
+    {
+        int numByUnit = num / unit;
+        num %= unit;
+        return numByUnit;
     }
     
     /**
