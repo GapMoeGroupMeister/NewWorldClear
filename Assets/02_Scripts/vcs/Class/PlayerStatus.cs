afa1cb9c175d00ca4d8487957ca4356f08676c79
vcsHB <99899703+vcsHB@users.noreply.github.com>
2023-12-25 14:01:22 +0900

    feat: PlayerStatus

--- Assets/02_Scripts/vcs/Class/PlayerStatus.cs
+++ Assets/02_Scripts/vcs/Class/PlayerStatus.cs
@@ -34,6 +34,38 @@ public class PlayerStatus
 	 */
 	public int levelHealth;
 
+	/**
+	 * <summary>
+	 * 기본 공격력
+	 * </summary>
+	 */
+	public int atk = 10;
+	/**
+	 * <summary>
+	 * 기본 방어력
+	 * </summary>
+	 */
+	public int def = 1;
+	/**
+	 * <summary>
+	 * 스태미너
+	 * </summary>
+	 */
+	public int stamina = 200;
+	/**
+	 * <summary>
+	 * 기본 이동 속도
+	 * </summary>
+	 */
+	public float moveSpeed = 1;
+	/**
+	 * <summary>
+	 * 인벤토리 슬롯 사이즈
+	 * </summary>
+	 */
+	public int inventorySize = 10;
+	
+
 	public static int Calc_HealthMax(int level)
 	{
 		int max = level * 5 + 100;
