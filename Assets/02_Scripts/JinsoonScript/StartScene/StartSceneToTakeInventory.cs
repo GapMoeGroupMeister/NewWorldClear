ea5d9e984bb06fac1bff699fb6c4e17fe29c49fc
leoo <jinhukim07@gmail.com>
2023-12-28 11:23:07 +0900

    fix:startScene

--- Assets/02_Scripts/JinsoonScript/StartScene/StartSceneToTakeInventory.cs
+++ Assets/02_Scripts/JinsoonScript/StartScene/StartSceneToTakeInventory.cs
@@ -1,10 +1,18 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 using UnityEngine.EventSystems;
 
 public class StartSceneToTakeInventory : MonoBehaviour, IPointerClickHandler
 {
+    public List<ItemSlot> inventory;
+    [SerializeField] private GameObject slotPrefab;
+    [SerializeField] private RectTransform grid;
+
+    public UnityEvent onClick;
+
     private GameObject outline;
 
     private void Awake()
@@ -21,5 +29,27 @@ public class StartSceneToTakeInventory : MonoBehaviour, IPointerClickHandler
     public void OnPointerClick(PointerEventData eventData)
     {
         StartSceneStorageInventoryManager.instance.ClickInventorySlot();
+        onClick?.Invoke();
+    }
+
+    public void SetItem(ItemSlot itemSlot)
+    {
+        inventory.Add(itemSlot);
+        GameObject slot = Instantiate(slotPrefab, grid);
+
+        slot.GetComponent<Slot>().SetSlot(itemSlot);
+    }
+
+    public void Delete_Inven()
+    {
+        foreach (Transform child in grid)
+        {
+            Destroy(child.gameObject);
+        }
+    }
+
+    public void OnStartGame()
+    {
+        DBManager.Save_InGameInventory(inventory);
     }
 }
